32b3da8331549a5fb5ef595278cec445b64a56b4
Chris Walker <cmw161@uclive.ac.nz>
2018-03-26 22:50:17 +1300

    Add a check to run to see if it is time to send updates

--- router.py
+++ router.py
@@ -3,6 +3,7 @@ from select import select
 from collections import OrderedDict
 import sys
 import json
+import time
 
 import config_loader
 
@@ -20,7 +21,7 @@ socket.__repr__ = simplified_socket_str
 class Router:
     INFINITY = 16
 
-    READ_TIMEOUT = 3  # How long the router should wait for sockets to be ready to be read from.
+    READ_TIMEOUT = 2  # How long the router should wait for sockets to be ready to be read from
 
     def __init__(self, config_lines):
         self.id = None
@@ -28,16 +29,18 @@ class Router:
         self.outputs = {}
         self.update_period = None
         self.timeout_length = None
-
+        # Assign all variables above
         self.config_loader = config_loader.Loader(config_lines, self)
         self.config_loader.load()
 
         self.input_sockets = {}
-
         self.routing_table = {}
+
+        self.time_of_last_update = time.time()
 
     def bind_sockets(self):
         """ Bind sockets to input ports """
+        # TODO: use try except block to gracefully handle socket errors such as ports already bound
         for input_port in self.input_ports:
             a_socket = socket(AF_INET, SOCK_DGRAM)
             a_socket.bind(("localhost", input_port))
@@ -45,10 +48,10 @@ class Router:
 
     def initialise_routing_table(self):
         """  Initialise the router's routing table """
-        with open("./routing-table-" + str(self.id) + ".json", "w+") as routing_table_file:
+        with open("./json-memory/routing-table-" + str(self.id) + ".json", "w+") as routing_table_file:
             if routing_table_file.readlines():
                 # routing_table_json = json.load(routing_table_file)
-                # TODO: check if this json contains the info from 'outputs'
+                # TODO: check if this json contains at least the info from 'outputs' (might contain more info)
                 pass
             else:
                 routing_table = {}
@@ -66,14 +69,20 @@ class Router:
 
     def run(self):
         """ Check and process incoming packets (select) and timing events."""
+        if time.time() - self.time_of_last_update >= self.update_period:
+            print("\tTime to send updates! --->")
+            self.time_of_last_update = time.time()
+            # TODO: send update message / packet
+            for output in self.outputs.values():
+                a_socket = socket(AF_INET, SOCK_DGRAM)
+                a_socket.sendto(b"Update", ("localhost", output[0]))
+
+        # TODO: parse and process any and all periodic updates from other routers
         read_ready = select(self.input_sockets.values(), [], [], self.READ_TIMEOUT)[0]
         for a_socket in read_ready:
             print("READ READY!", a_socket)
-            buffer = a_socket.recv(3)
+            buffer = a_socket.recv(6)
             print("RECEIVED:", buffer)
-        for output in self.outputs.values():
-            a_socket = socket(AF_INET, SOCK_DGRAM)
-            a_socket.sendto(b"Hey", ("localhost", output[0]))
 
 
 def main():
@@ -83,16 +92,15 @@ def main():
         return
 
     config_filename = args[1]
-    config_file = open(config_filename, "r")
-    config_lines = config_file.readlines()
-    config_file.close()
+    with open(config_filename, "r") as config_file:
+        config_lines = config_file.readlines()
 
     router = Router(config_lines)
     router.bind_sockets()
     router.initialise_routing_table()
 
-    # while True:
-    #     router.run()
+    while True:
+        router.run()
 
 if __name__ == "__main__":
     main()
