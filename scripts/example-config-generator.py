936efc40c3791ecc09b9599881397e10bbe914cb
Chris Walker <cmw161@uclive.ac.nz>
2018-04-23 16:21:56 +1200

    Fix costs logic to be consistent within an undirected network. Add logic to create adjacency and incidence matrices, for better visualisation

--- scripts/example-config-generator.py
+++ scripts/example-config-generator.py
@@ -1,5 +1,6 @@
 import random
 import os
+from collections import OrderedDict
 
 inp = """
 1:2,3,4,8
@@ -10,6 +11,17 @@ inp = """
 6:1
 """
 example_num = "2"
+update_period = "5"
+min_cost = 1
+max_cost = 4
+
+
+class Edge(frozenset):
+    def __str__(self):
+        return set(map(str, self))
+
+    def __repr__(self):
+        return str(set(map(str, self)))
 
 config_path = "../configurations/example-" + example_num + "/"
 if os.path.isdir(config_path):
@@ -17,38 +29,73 @@ if os.path.isdir(config_path):
     if confirm != "y":
         exit()
 
-connections = {}
-
+edge_costs = {}
 for line in inp.strip().splitlines():
     line = line.strip()
     parts = line.split(":")
     router = parts[0]
+    neighbours = set(parts[1].split(","))
+    for neighbour in neighbours:
+        edge = Edge(frozenset([router, neighbour]))
+        edge_costs[edge] = random.randint(min_cost, max_cost)
+edge_costs = OrderedDict(edge_costs)
+
+connections = {}
+for line in inp.strip().splitlines():
+    line = line.strip()
+    parts = line.split(":")
+    router = parts[0]
+    neighbours = set(parts[1].split(","))
     if router not in connections:
-        connections[router] = set(parts[1].split(","))
+        connections[router] = neighbours
     else:
-        connections[router] |= set(parts[1].split(","))
-    for connection in parts[1].split(","):
-        if connection not in connections:
-            connections[connection] = set(router)
+        connections[router] |= neighbours
+    for neighbour in neighbours:
+        if neighbour not in connections:
+            connections[neighbour] = set(router)
         else:
-            connections[connection] |= set(router)
+            connections[neighbour] |= set(router)
+
+num_routers = max(map(int, connections.keys()))
+adj_matrix = [[0 for i in range(num_routers)] for j in range(num_routers)]
+for router, neighbours in connections.items():
+    for neighbour in neighbours:
+        adj_matrix[int(router) - 1][int(neighbour) - 1] = 1
+
+edges = list(edge_costs.keys())
+num_edges = len(edge_costs.keys())
+inc_matrix = [[0 for k in range(num_edges)] for l in range(num_routers)]
+for edge, cost in edge_costs.items():
+    edge_num = edges.index(edge)
+    edge_list = list(edge)
+    node1 = edge_list[0]
+    node2 = edge_list[1]
+    inc_matrix[int(node1) - 1][edge_num] = cost
+    inc_matrix[int(node2) - 1][edge_num] = cost
 
 for router in connections:
     config = ""
     config += "router-id " + router + "\n"
     config += "input-ports "
-    for connection in connections[router]:
-        config += "90" + router + connection + ", "
+    for neighbour in connections[router]:
+        config += "90" + router + neighbour + ", "
     config = config[:-2]
     config += "\noutputs "
-    for connection in connections[router]:
-        randcost = random.randint(1, 4)
-        config += "90" + connection + router + "/" + str(randcost) + "/" + connection + ", "
+    for neighbour in connections[router]:
+        cost = edge_costs[Edge(frozenset([router, neighbour]))]
+        config += "90" + neighbour + router + "/" + str(cost) + "/" + neighbour + ", "
     config = config[:-2]
-    config += "\nupdate-period 5"
+    config += "\nupdate-period " + update_period
     config_filename = "example-" + example_num + "-config-" + router + ".txt"
     os.makedirs(os.path.dirname(config_path), exist_ok=True)
     with open(config_path + config_filename, "w+") as config_file:
         config_file.write(config)
 
-
+print("\nConfig files successfully created.\n")
+print("VISUALISE USING THIS ONLINE TOOL: http://graphonline.ru/en/")
+print("\nADJACENCY MATRIX:")
+for line in adj_matrix:
+    print(",".join(map(str, line)))
+print("\nINCIDENCE MATRIX:")
+for line in inc_matrix:
+    print(",".join(map(str, line)))
