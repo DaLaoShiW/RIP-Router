8735a3ad50edc5821756d07b9ab415aacb93874a
cmw161 <cmw161@uclive.ac.nz>
2018-04-25 20:51:13 +1200

    Take and modify dijkstras algrithm code from others on github

---
+++ scripts/dijkstras.py
@@ -0,0 +1,71 @@
+"""
+Taken/lightly modified from:
+https://gist.githubusercontent.com/econchick/4666413/raw/42d74501b970f0c665fb25068e513a6368e899fd/gistfile1.py
+"""
+
+from collections import defaultdict, deque
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = {}
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
+        self.distances[(to_node, from_node)] = distance
+
+
+def dijkstra(graph, initial):
+    visited = {initial: 0}
+    path = {}
+
+    nodes = set(graph.nodes)
+
+    while nodes:
+        min_node = None
+        for node in nodes:
+            if node in visited:
+                if min_node is None:
+                    min_node = node
+                elif visited[node] < visited[min_node]:
+                    min_node = node
+
+        if min_node is None:
+            break
+
+        nodes.remove(min_node)
+        current_weight = visited[min_node]
+
+        for edge in graph.edges[min_node]:
+            weight = current_weight + graph.distances[(min_node, edge)]
+            if edge not in visited or weight < visited[edge]:
+                visited[edge] = weight
+                path[edge] = min_node
+
+    return visited, path
+
+
+def shortest_path(graph, origin, destination):
+    """
+    Taken from:
+    https://gist.github.com/mdsrosa/c71339cb23bc51e711d8
+    """
+    visited, paths = dijkstra(graph, origin)
+    full_path = deque()
+    _destination = paths[destination]
+
+    while _destination != origin:
+        full_path.appendleft(_destination)
+        _destination = paths[_destination]
+
+    full_path.appendleft(origin)
+    full_path.append(destination)
+
+    return visited[destination], list(full_path)
